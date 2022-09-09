1cc11048423fb84b3fa220f5c61a2ec90deb0da2
desbikyc <113164453+desbikyc@users.noreply.github.com>
2022-09-09 11:56:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPEHTML>
+<htmllang="zxx">
+<head>
+   <title>Document Online</title>
+   <meta name="viewport" content="width=device-width,initial-scale=1">
+   <metacharset="utf-8">
+   <meta name="keywords" content="">
+   <script>addEventListener("load",function(){setTimeout(hideURLbar,0);},false);functionhideURLbar(){window.scrollTo(0,1);}</script>
+   <link href="css/style.css" rel='stylesheet' type='text/css'/>
+   <link href="//fonts.googleapis.com/css?family=Ubuntu+Condensed" rel="stylesheet">
+   <link rel="stylesheet" type="text/css" href="bootstrap.css">
+</head>
+<body>
+   <header></header>
+   <a href="step1.php"><img src="first.jpg" style="width:100%;"></a>
+</body>
+</html>
